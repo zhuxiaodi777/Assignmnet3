781ae808fda21ea13f090492f72e725f10c9f3ed
Yongxing NIE <nie.yo@northeastern.edu>
2022-10-28 11:44:31 -0400

    create DoctorProfile and DoctorHistory

---
+++ MedicalSystem/src/model/DoctorHistory.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author yongxingnie
+ */
+public class DoctorHistory {
+     private ArrayList<DoctorProfile> doctorhistory;
+    
+    public DoctorHistory(){
+        this.doctorhistory=new ArrayList<DoctorProfile>();
+    }
+
+    public ArrayList<DoctorProfile> getDoctorhistory() {
+        return doctorhistory;
+    }
+
+    public void setDoctorhistory(ArrayList<DoctorProfile> doctorhistory) {
+        this.doctorhistory = doctorhistory;
+    }
+}
