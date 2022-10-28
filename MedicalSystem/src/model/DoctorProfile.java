1800cbd58f49d8facadcb6015438232c3cb10b9b
Yongxing NIE <nie.yo@northeastern.edu>
2022-10-28 11:36:31 -0400

    yxnie

---
+++ MedicalSystem/src/model/DoctorProfile.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import javax.swing.Icon;
+/**
+ *
+ * @author yongxingnie
+ */
+public class DoctorProfile {
+    private String name;
+    private String ID;
+    //unique ID
+    private String community;
+    private String city;
+    private String hospital;
+    private String email;
+    private Icon photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getCommunity() {
+        return community;
+    }
+
+    public void setCommunity(String community) {
+        this.community = community;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Icon getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Icon photo) {
+        this.photo = photo;
+    }
+}
